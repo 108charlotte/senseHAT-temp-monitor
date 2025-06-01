df25982b92f045cdf0625d6a68e18225bb3420ba
108charlotte <152224795+108charlotte@users.noreply.github.com>
2025-06-01 19:57:23 +0000

    check temperature of cpu, determine senseHAT colors accordingly and set LED matrix

---
+++ main.py
@@ -0,0 +1,22 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+def get_cpu_temperature(): 
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        temp = f.read()
+    return float(temp) / 1000.0
+
+def get_color_from_temperature(temp):
+    if temp < 55:
+        return (0, 255, 0) # green
+    elif temp < 64:
+        return (255, 255, 0)  # yellow
+    elif temp < 74:
+        return (255, 165, 0)  # orange
+    else:
+        return (255, 0, 0)  # red
+
+cpu_temp = get_cpu_temperature()
+
+sense.clear(get_color_from_temperature(cpu_temp))
---
+++ resources.md
@@ -0,0 +1,5 @@
+which file temp is in: https://forums.raspberrypi.com/viewtopic.php?t=309480
+
+how to read from that file: https://www.w3schools.com/python/python_file_open.asp
+
+manipulate senseHAT LED matrix: https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/5
